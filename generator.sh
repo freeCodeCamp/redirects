93c71bf21ae9b62c5e444d7edb01c86be8ce4dae
Mrugesh Mohapatra <me@raisedadead.com>
2019-08-19 03:01:47 +0530

    fix: update generator

--- generator.sh
+++ generator.sh
@@ -1,1 +1,2 @@
-mkdir $1.freecodecamp.org & echo "/* https://www.freecodecamp.org/news/$1/:splat" >> $1.freecodecamp.org/_redirects
+mkdir $1.freecodecamp.org
+echo "/* https://www.freecodecamp.org/news/$1/:splat" >> $1.freecodecamp.org/_redirects
